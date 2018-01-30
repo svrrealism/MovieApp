eabf02359a2d699d146fda85991de97f26fa2af4
svrrealism <svrrealists@gmail.com>
2018-01-30 18:55:38 -0500

    Create migration data to form the columns for actors, movies, and parts.

--- db/migrate/20180130165750_create_movies.rb
+++ db/migrate/20180130165750_create_movies.rb
@@ -1,7 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.1]
   def change
     create_table :movies do |t|
-
+      t.string :title
+      t.string :image
+      t.string :release_year
+      t.string :plot
       t.timestamps
     end
   end
