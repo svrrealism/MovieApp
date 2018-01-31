9a7c214a4ae6a7587e53abb0125373bf5559d8a3
svrrealism <svrrealists@gmail.com>
2018-01-30 21:20:46 -0500

    Add functionality to show all actors, if individual then also all attending movies.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/movies' => 'movies#index'
   get '/movies/:id' => 'movies#show', as: :movie
-  get' /actors/:id' => 'actors#show', as: :actor
+  get '/actors' => 'actors#index'
+  get '/actors/:id' => 'actors#show', as: :actor
 end
